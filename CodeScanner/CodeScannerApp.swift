fd5628bbeb831699cf23e30687c5e7141609174b
rivaldofez <rivaldofez@gmail.com>
2022-11-11 18:48:31 +0700

    Initial Commit

---
+++ CodeScanner/CodeScannerApp.swift
@@ -0,0 +1,17 @@
+//
+//  CodeScannerApp.swift
+//  CodeScanner
+//
+//  Created by Rivaldo Fernandes on 11/11/22.
+//
+
+import SwiftUI
+
+@main
+struct CodeScannerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
